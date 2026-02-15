d9d282beabb0a932eca49dddc0dd3bd3823c9338
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-02-15 23:51:39 +0600

    Remove Existing santry page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 AH JIM
+          Copyright © 2026 AH JIM
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
